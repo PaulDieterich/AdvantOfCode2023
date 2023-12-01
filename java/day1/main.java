8dfb0ed0f56b339c4aa2d2e4b4b86f8d076c6188
PaulDieterich <paul.dieterich@gmail.com[A>
2023-12-01 22:14:43 +0100

    day1 in java

---
+++ java/day1/main.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class main{
+
+    public static void main(String[] args){
+            
+        try{
+            Scanner scanner = new Scanner(new File("inputFile.txt"));        
+            int total = 0;
+            while (scanner.hasNextLine()) {
+                char[] characters = scanner.nextLine().toCharArray();
+                Integer sum = 0;
+                String firstNumber = "";
+                String lastNumber = "";            
+                boolean firstNumberFound = false;
+
+                for(int i = 0; i < characters.length; i++){
+                    if(Character.isDigit(characters[i])){
+                        if(!firstNumberFound){
+                            firstNumberFound = true;
+                            firstNumber = String.valueOf(characters[i]);
+                            lastNumber = String.valueOf(characters[i]); 
+                        }else{
+                            lastNumber = String.valueOf(characters[i]);
+                        }
+                    }
+                    
+                }
+                sum = Integer.parseInt(firstNumber+""+lastNumber);
+                total += sum;        
+            }
+            scanner.close();
+            System.out.println(total);
+        
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    } 
+
+}
