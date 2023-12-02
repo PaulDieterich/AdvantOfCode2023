50963a5c0fd383d0fec284581fc560a63372fb7f
PaulDieterich <paul.dieterich@gmail.com[A>
2023-12-02 10:38:07 +0100

    rename

--- java/day1/Day1.java
+++ java/day1/Day1.java
@@ -1,8 +1,9 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class main{
+public class Day1{
 
     public static void main(String[] args){
             
